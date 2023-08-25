8c4b405c351bb0e690780d04729417f736c95a32
NoeDenise <noelialombardo@hotmail.com>
2023-08-25 17:57:37 -0300

    tratando de ver las partidas

--- show_data.js
+++ show_data.js
@@ -1,75 +1,66 @@
 import { partidaServices } from "./service/partidas-service.js";
-import {  validarPartNacimiento,
-    validarPartidaMatrimonio,
-    validarEdadMatrimonio,
-    validarNacionalidad,
-     validarNacionalidadDefuncion} from "./validaciones.js"
+// import {  validarPartNacimiento,
+//     validarPartidaMatrimonio,
+//     validarEdadMatrimonio,
+//     validarNacionalidad,
+//      validarNacionalidadDefuncion} from "./validaciones.js"
 
 console.log(partidaServices);
 
 //TODO: Ver como se muestan los datos: bday/place/Age.. ponerlo en español
 
-let partidasData
+//let partidasData
 
-document.addEventListener("DOMContentLoaded", async () => {
-     partidasData = await partidaServices.listaPartidas();
-    console.log("Datos de partidas obtenidos:", partidasData);
-
-    // Aquí puedes llamar a una función que construya y muestre el contenido HTML de las partidas
-    mostrarPartidasGuardadas(partidasData);
-   
-});
-
-function mostrarPartidasGuardadas(partidas) {
+// show_data.js
+document.addEventListener("DOMContentLoaded", () => {
     const partidasContainer = document.getElementById("partidas-container");
 
-    partidas.forEach(partida => {
-        const persona = Object.keys(partida)[0];
-        const partidaDetails = partida[persona];
-        
-        const partidaDiv = document.createElement("div");
-        partidaDiv.classList.add("partida");
+    const partidasGuardadas = JSON.parse(localStorage.getItem("partidasGuardadas")) || [];
 
-        const nombreHeading = document.createElement("h1");
-        nombreHeading.textContent = persona;
-        partidaDiv.appendChild(nombreHeading);
+    partidasGuardadas.forEach((partida, index) => {
+        const personaName = Object.keys(partida)[0]; // Obtiene el nombre de la persona
+        const partidaData = partida[personaName];
 
-        for (const tipoPartida in partidaDetails) {
-            const tipoPartidaHeading = document.createElement("h3");
-            tipoPartidaHeading.textContent = tipoPartida;
-            partidaDiv.appendChild(tipoPartidaHeading);
-        
-            const datosPartida = partidaDetails[tipoPartida];
+        const personaElement = document.createElement("div");
+        personaElement.classList.add("persona");
+
+        const nombreElement = document.createElement("h2");
+        nombreElement.textContent = personaName;
+
+        personaElement.appendChild(nombreElement);
+
+        for (const tipoPartida in partidaData) {
+            const tipoPartidaElement = document.createElement("h3");
+            tipoPartidaElement.textContent = tipoPartida;
+
+            personaElement.appendChild(tipoPartidaElement);
+
+            const datosPartida = partidaData[tipoPartida];
             for (const campo in datosPartida) {
-                const campoParrafo = document.createElement("p");
-                if (campo === "hijoDe") {
-                    const hijoDe = datosPartida[campo];
-                    const hijoDeTexto = `Hijo de: ${hijoDe.madre} y ${hijoDe.padre}`;
-                    campoParrafo.textContent = hijoDeTexto;
-                } else {
-                    campoParrafo.textContent = `${campo}: ${datosPartida[campo]}`;
-                }
-                partidaDiv.appendChild(campoParrafo);
+                const campoElement = document.createElement("p");
+                campoElement.textContent = `${campo}: ${datosPartida[campo]}`;
+
+                personaElement.appendChild(campoElement);
             }
         }
 
-        partidasContainer.appendChild(partidaDiv);
+        partidasContainer.appendChild(personaElement);
     });
-}
-
-const validarPartidasButton = document.getElementById("validar-partidas");
-validarPartidasButton.addEventListener("click", () => {
-    console.log("Hice click en validar partidas");
-    console.log("Listado de partidas guardadas:", partidasData); // Accedemos al listado de partidasData
- 
-    validarPartNacimiento(partidasData);
-    validarPartidaMatrimonio(partidasData)
-    validarEdadMatrimonio(partidasData);
-    validarNacionalidad(partidasData);
-     validarNacionalidadDefuncion(partidasData)
 });
 
+// const validarPartidasButton = document.getElementById("validar-partidas");
+// validarPartidasButton.addEventListener("click", () => {
+//     console.log("Hice click en validar partidas");
+//     console.log("Listado de partidas guardadas:", partidasData); // Accedemos al listado de partidasData
+ 
+//     validarPartNacimiento(partidasData);
+//     validarPartidaMatrimonio(partidasData)
+//     validarEdadMatrimonio(partidasData);
+//     validarNacionalidad(partidasData);
+//      validarNacionalidadDefuncion(partidasData)
+// });
 
-export {partidasData}
+
+// export {partidasData}
 
     
