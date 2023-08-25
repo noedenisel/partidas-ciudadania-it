4202b6c532a43cbc07449fe04fe28f3c72cacf6d
NoeDenise <noelialombardo@hotmail.com>
2023-08-25 12:56:10 -0300

    guarda ok en local del usuario

--- service/partidas-service.js
+++ service/partidas-service.js
@@ -1,34 +1,30 @@
 // console.log("client service conectado");
 
 async function savePartida(dbData) {
-    console.log("Datos a enviar al servidor:", dbData);
-
-   try {
-       const response = await fetch('https://partidas-ciudadania-it.vercel.app/partidas', {
-           method: 'POST',
-           headers: {
-               'Content-Type': 'application/json'
-           },
-           body: JSON.stringify(dbData)
-       });
-       return await response.json();
-   } catch (error) {
-       console.error("Error al enviar los datos:", error);
-   }
+     console.log("Datos a enviar al servidor:", dbData);
+ 
+    try {
+        const response = await fetch('http://localhost:3000/partidas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dbData)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error al enviar los datos:", error);
+    }
 }
 
 const listaPartidas = () => 
-   fetch("https://partidas-ciudadania-it.vercel.app/partidas")
-   .then(respuesta => {
-       console.log("Respuesta del servidor:", respuesta);
-       return respuesta.json();
-   });
+    fetch("http://localhost:3000/partidas")
+    .then( respuesta => respuesta.json()
+  )
 
 
 export const partidaServices = {
-   savePartida,
-   listaPartidas
+    savePartida,
+    listaPartidas
 }
-
-
 
