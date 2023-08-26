5806c4b674350eed38990878fb74a7dc8ca16f4b
NoeDenise <noelialombardo@hotmail.com>
2023-08-26 15:31:36 -0300

    normaliza nombres cuando los captura

--- matrimonio.js
+++ matrimonio.js
@@ -1,37 +1,47 @@
 // <!----Agregar Matrimonio----!>
+
+
 const marriageForm = document.querySelector("[data-marriage-form]");
 
-export function obtenerDatosPartidaMatrimonio () {
+// Función para normalizar nombres y apellidos
+function normalizeName(name) {
+    const words = name.toLowerCase().split(/[ /]/);
+    const normalized = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+    return normalized;
+}
 
+export function obtenerDatosPartidaMatrimonio() {
     const name = marriageForm.querySelector("[data-name]").value;
-    const lastname = marriageForm.querySelector("[data-lastname]").value
+    const lastname = marriageForm.querySelector("[data-lastname]").value;
     const age = marriageForm.querySelector("[data-age]").value;
     const nacionalidad = marriageForm.querySelector("[data-nacionalidad]").value;
     const bdayPlace = marriageForm.querySelector("[data-bday-place]").value;
     const motherName = marriageForm.querySelector("[data-mother-name]").value;
-    const motherlastname = marriageForm.querySelector("[data-mother-lastname]").value;
+    const motherLastname = marriageForm.querySelector("[data-mother-lastname]").value;
     const fatherName = marriageForm.querySelector("[data-father-name]").value;
-    const fatherlastname = marriageForm.querySelector("[data-father-lastname]").value;
+    const fatherLastname = marriageForm.querySelector("[data-father-lastname]").value;
     const conyugeName = marriageForm.querySelector("[data-conyuge-name]").value;
-    const conyugelastname = marriageForm.querySelector("[data-conyuge-lastname]").value;
-    
-    const marriageDate = marriageForm.querySelector("[data-marriage-date]").value
+    const conyugeLastname = marriageForm.querySelector("[data-conyuge-lastname]").value;
+
+    const marriageDate = marriageForm.querySelector("[data-marriage-date]").value;
     const marriagePlace = marriageForm.querySelector("[data-marriage-place]").value;
 
-    const dataMatrimonio ={
-        persona : name + " " +lastname,
+  
+
+    const dataMatrimonio = {
+        persona: normalizeName(name) + " " + normalizeName(lastname),
         hijoDe: {
-            madre: motherName + " " + motherlastname ,
-            padre: fatherName + " " + fatherlastname
+            madre: normalizeName(motherName) + " " + normalizeName(motherLastname),
+            padre: normalizeName(fatherName) + " " + normalizeName(fatherLastname)
         },
         age: age,
-        nacionalidad: nacionalidad,
-        bdayPlace: bdayPlace,
-        conyuge:conyugeName + " " + conyugelastname,
+        nacionalidad: normalizeName(nacionalidad),
+        bdayPlace: normalizeName(bdayPlace),
+        conyuge: normalizeName(conyugeName) + " " + normalizeName(conyugeLastname),
         date: marriageDate,
-        place: marriagePlace
-    }
+        place: normalizeName(marriagePlace)
+    };
 
     console.log("Datos a enviar partida de matrimonio:", dataMatrimonio);
-    return dataMatrimonio
+    return dataMatrimonio;
 }
--- partida.js
+++ partida.js
@@ -1,25 +1,33 @@
-const form = document.querySelector("[data-avo-form]");
-// console.log(form);
+// Función para normalizar nombres y apellidos
+function normalizeName(name) {
+    const words = name.toLowerCase().split(/[ /]/);
+    const normalized = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+    return normalized;
+}
 
+// Obtener datos de la partida de nacimiento
 export function obtenerDatosPartidaAvo() {
     const name = document.querySelector("[data-avo-name]").value;
     const lastname = document.querySelector("[data-avo-lastname]").value;
-    const bdayDate = document.querySelector("[data-avo-bday-date]").value
+    const bdayDate = document.querySelector("[data-avo-bday-date]").value;
     const birthPlace = document.querySelector("[data-avo-place]").value;
     const motherName = document.querySelector("[data-avo-mother-name]").value;
     const motherLastname = document.querySelector("[data-avo-mother-lastname]").value;
     const fatherName = document.querySelector("[data-avo-father-name]").value;
     const fatherLastname = document.querySelector("[data-avo-father-lastname]").value;
 
-    const persona = name + " " + lastname;
+ 
+
+    const persona = normalizeName(name) + " " + normalizeName(lastname)
 
     const datosPartida = {
-        name: name, // Agrega el campo name aquí
+        name: normalizeName(name),
+        lastname: normalizeName(lastname),
         persona: persona,
         bday: bdayDate,
-        lugarNacimiento: birthPlace,
-        madre: motherName + " " + motherLastname,
-        padre: fatherName + " " + fatherLastname,
+        lugarNacimiento: normalizeName(birthPlace),
+        madre: normalizeName(motherName) + " " + normalizeName(motherLastname),
+        padre: normalizeName(fatherName) + " " + normalizeName(fatherLastname),
     };
 
     console.log("Datos a enviar partida de nacimiento:", datosPartida);
